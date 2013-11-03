8ecd8249734ebfab20aa9250d79f4434909b9569
moreyw <morey.wood@gmail.com>
2013-11-03 15:47:36 -0600

    Filled in accountCreation

--- Tarsus/src/java/GameInstance.java
+++ Tarsus/src/java/GameInstance.java
@@ -12,6 +12,7 @@ public class GameInstance {
     AresCharacter aresChar;
     stateEnum currentState;
     String accountName;
+    stateEnum startingState;
     
     GameInstance()
     {
@@ -240,7 +241,103 @@ public class GameInstance {
      * @return the next state
      ***************************************************/
     stateEnum accountCreation(PrintWriter out, HttpServletRequest request) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String accountPage = "<html>\n" +
+            "	<head>\n" +
+            "	<!-- Call normalize.css -->\n" +
+            "	<link rel=\"stylesheet\" href=\"css/normalize.css\" type=\"text/css\" media=\"screen\">\n" +
+            "	<!-- Import Font to be used in titles and buttons -->\n" +
+            "	<link href='http://fonts.googleapis.com/css?family=Sanchez' rel='stylesheet' type='text/css'>\n" +
+            "	<link href='http://fonts.googleapis.com/css?family=Prosto+One' rel='stylesheet' type='text/css'>\n" +
+            "	<!-- Call style.css -->\n" +
+            "	<link rel=\"stylesheet\" href=\"css/grid.css\" type=\"text/css\" media=\"screen\">\n" +
+            "	<!-- Call style.css -->\n" +
+            "	<link rel=\"stylesheet\" href=\"css/style.css\" type=\"text/css\" media=\"screen\">\n" +
+            "	<title> Tarsus </title>\n" +
+            "	</head>\n" +
+            "	<div id=\"header\" class=\"grid10\" align=\"right\"> \n" +
+            "		<a href=\"index.html\" id=\"tarsusTitle\"> TARSUS </a> \n" +
+            "		<a class=\"button\" href=\"login.html\"> Log In </a> </div>\n" +
+            "	<div class=\"grid1\"> </div>\n" +
+            "	<div class=\"grid8 centered\">\n" +
+            "		<h1 id=\"title\" class=\"centered\"> Sign Up Below</h1>\n" +
+            "		<form method=\"post\" action=\"Tarsus\"> \n" +
+            "			<p align=\"center\"> \n" +
+            "				Username: <input name=\"username\" type=\"text\" /> \n" +
+            "			</p>\n" +
+            "			<p align=\"center\"> \n" +
+            "				Password: <input name=\"password\" type=\"password\" /> \n" +
+            "			</p>\n" +
+            "			<p align=\"center\"> \n" +
+            "				Confirm Password: <input name=\"confirmpassword\" type=\"password\" /> \n" +
+            "			</p>\n" +
+            "			<p align=\"center\"> \n" +
+            "				<input class=\"signUpButton\" value=\"Sign Up\" type=\"submit\"/> \n" +
+            "			</p>\n" +
+            "		</form>\n" +
+            "	</div>\n" +
+            "	<div class=\"grid1\"> </div>\n" +
+            "	\n" +
+            "</html>";
+        String passwordError = "<html>\n" +
+            "	<head>\n" +
+            "	<!-- Call normalize.css -->\n" +
+            "	<link rel=\"stylesheet\" href=\"css/normalize.css\" type=\"text/css\" media=\"screen\">\n" +
+            "	<!-- Import Font to be used in titles and buttons -->\n" +
+            "	<link href='http://fonts.googleapis.com/css?family=Sanchez' rel='stylesheet' type='text/css'>\n" +
+            "	<link href='http://fonts.googleapis.com/css?family=Prosto+One' rel='stylesheet' type='text/css'>\n" +
+            "	<!-- Call style.css -->\n" +
+            "	<link rel=\"stylesheet\" href=\"css/grid.css\" type=\"text/css\" media=\"screen\">\n" +
+            "	<!-- Call style.css -->\n" +
+            "	<link rel=\"stylesheet\" href=\"css/style.css\" type=\"text/css\" media=\"screen\">\n" +
+            "	<title> Tarsus </title>\n" +
+            "	</head>\n" +
+            "	<div id=\"header\" class=\"grid10\" align=\"right\"> \n" +
+            "		<a href=\"index.html\" id=\"tarsusTitle\"> TARSUS </a> \n" +
+            "		<a class=\"button\" href=\"login.html\"> Log In </a> </div>\n" +
+            "	<div class=\"grid1\"> </div>\n" +
+            "	<div class=\"grid8 centered\">\n" +
+            "		<h1 id=\"title\" class=\"centered\"> Sign Up Below</h1>\n" +
+                " <h3 id=\"title\" class=\"centered\"> Passwords do not match </h3> \n" +
+            "		<form method=\"post\" action=\"Tarsus\"> \n" +
+            "			<p align=\"center\"> \n" +
+            "				Username: <input name=\"username\" type=\"text\" /> \n" +
+            "			</p>\n" +
+            "			<p align=\"center\"> \n" +
+            "				Password: <input name=\"password\" type=\"password\" /> \n" +
+            "			</p>\n" +
+            "			<p align=\"center\"> \n" +
+            "				Confirm Password: <input name=\"confirmpassword\" type=\"password\" /> \n" +
+            "			</p>\n" +
+            "			<p align=\"center\"> \n" +
+            "				<input class=\"signUpButton\" value=\"Sign Up\" type=\"submit\"/> \n" +
+            "			</p>\n" +
+            "		</form>\n" +
+            "	</div>\n" +
+            "	<div class=\"grid1\"> </div>\n" +
+            "	\n" +
+            "</html>";
+        if(startingState != stateEnum.ACCOUNT_CREATION)
+        {
+            out.println(accountPage);
+            return stateEnum.ACCOUNT_CREATION;
+        }
+        else{
+            String username = request.getParameter("username");
+
+            /*if(!isValidString(username))
+            {
+
+            }*/
+            int password = request.getParameter("password").hashCode();
+            int confirmPassword = request.getParameter("confirmpassword").hashCode();
+            if(password != confirmPassword){
+                out.println(passwordError);
+                return stateEnum.ACCOUNT_CREATION;  
+            }
+            String command = "INSERT INTO Login VALUES (" + username + ", "
+                    + password +");";
+            return stateEnum.LOGIN;
+        }
     }
     
 }
